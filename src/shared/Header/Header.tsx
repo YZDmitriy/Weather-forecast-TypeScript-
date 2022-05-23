ff8ba7ea6ca73c776bd88460ae263b1cba201a96
Dmitriy <dyuzef@mail.ru>
2022-05-23 19:44:22 +0300

    done header

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -14,17 +14,41 @@ function Header(props: Props) {
     { value: 'city-3', label: 'Новгород' },
   ];
 
+  const colourStyles = {
+    control: (styles: any) => ({
+      ...styles,
+      // backgroundColor:
+      //   theme.theme === Theme.DARK ? '#4F4F4F' : 'rgba(71, 147, 255, 0.2)',
+      width: '194px',
+      height: '37px',
+      border: 'none',
+      borderRadius: '10px',
+      zIndex: 100,
+    }),
+    // singleValue: (styles: any) => ({
+    //   ...styles,
+    //   color: theme.theme === Theme.DARK ? '#fff' : '#000',
+    // }),
+  };
+
+  // function changeTheme() {
+  //   theme.changeTheme(theme.theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+  // }
+
   return (
     <header className={s.header}>
       <div className={s.wrapper}>
         <div className={s.logo}><GlobalSvgSelector id='header-logo' /></div>
-      </div>
       <div className={s.title}>Weather forecast</div>
+      </div>
       <div className={s.wrapper}>
         <div className={s.change_theme} >
           <GlobalSvgSelector id="change-theme" />
         </div>
-          <Select options={options}/>
+          <Select 
+          styles={colourStyles} 
+          options={options}
+          defaultValue={options[0]}/>
       </div>
     </header>
   );
